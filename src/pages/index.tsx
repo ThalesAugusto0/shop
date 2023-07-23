46ea079e4bb0ebd26ed8f70a4b99ccc5220b1900
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-07-23 15:10:52 -0300

    Criado a estilização global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+});
+
 export default function Home() {
-  return <h1>Ola mundo</h1>;
+  return (
+    <>
+      <title>Ignite Shop</title>
+    </>
+  );
 }
