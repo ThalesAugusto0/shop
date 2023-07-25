c67eb02ca91cc719dc80b62cc7c4c77475836234
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-07-24 21:51:46 -0300

    Criado o carrossel com os produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,27 @@ import { styled } from "@/styles";
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
 
+import { useKeenSlider } from "keen-slider/react";
+
 import camisa1 from "../assets/1.png";
 import camisa2 from "../assets/2.png";
 import camisa3 from "../assets/3.png";
 
+import "keen-slider/keen-slider.min.css";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
+    <HomeContainer ref={sliderRef} className="keen-slider">
       <title>Ignite Shop</title>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camisa1} width={520} height={480} alt="" />
 
         <footer>
@@ -20,7 +31,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camisa2} width={520} height={480} alt="" />
 
         <footer>
@@ -29,7 +40,16 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta G</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image src={camisa3} width={520} height={480} alt="" />
 
         <footer>
